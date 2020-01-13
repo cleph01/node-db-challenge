be7857c44350777cde20c5e1a43c03ffd96ea974
Charlie <charles.j.montoya@gmail.com>
2020-01-13 15:53:28 -0500

    Essay Questions answered

--- ingredients/ingredients-model.js
+++ ingredients/ingredients-model.js
@@ -2,10 +2,10 @@ const db = require('../data/db-config');
 
 function getRecipesWithThisIngredient(ingredient_id){
 
-    return db(  'recipes'  )
-        .join( 'recipe_igredients', 'recipes.id', 'recipe_ingredients.recipe_id' )
-        .where( 'recipe_ingredients.id', ingredient_id )
-        .select('recipes.name');
+    return db(  'recipes as r'  )
+        .join( 'recipe_igredients as ri', 'r.id', 'ri.recipe_id' ) 
+        // .select('r.name')
+        .where('ri.recipe_id', '=', ingredient_id);
        
 }
 
